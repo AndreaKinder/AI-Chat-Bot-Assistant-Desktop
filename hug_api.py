8ab6609e753b85eb6cdcada9137d64d756d36b9c
AndreaKinder <andvillart@gmail.com>
2024-04-19 11:42:26 +0200

    Alpha app Chat Bot

--- hug_api.py
+++ hug_api.py
@@ -1,17 +1,31 @@
 from hugchat import hugchat
 from hugchat.login import Login
-from hugchat.hugchat import Conversation
+from create_log import read_log
+from check_log import check_file_log
+
+def import_loog():
+    if check_file_log():
+        email, passwd = read_log()
+        return email, passwd
+    else:
+        print("No se ha encontrado el fichero de log")
+        return None, None
 
 
-EMAIL = "andreavillartr@gmail.com"
-PASSWD = "HK3pJde@feAQge"
-cookie_path_dir = "./cookies/"
-sign = Login(EMAIL, PASSWD)
-cookies = sign.login(cookie_dir_path=cookie_path_dir, save_cookies=True)
+def create_cookies():
+    EMAIL, PASSWD = import_loog()
+    cookie_path_dir = "./cookies/"
+    sign = Login(EMAIL, PASSWD)
+    cookies = sign.login(cookie_dir_path=cookie_path_dir, save_cookies=True)
+    return cookies
 
-chatbot = hugchat.ChatBot(cookies=cookies.get_dict())
+def create_chatbot():
+    cookies = create_cookies()
+    chatbot = hugchat.ChatBot(cookies=cookies.get_dict())
+    return chatbot
 
 async def chat_init(mensaje):
+    chatbot = create_chatbot()
     print(f"Iniciando chat con mensaje: {mensaje}")
     id_chat = chatbot.get_conversation_from_id("Chat GPT")
     query_result:str = chatbot.chat(text=mensaje, model="Chat GPT", system_prompt="Assitente personal", conversation=id_chat)
@@ -20,8 +34,9 @@ async def chat_init(mensaje):
 
 
 async def chat_continuo(mensaje, id_chat):
+    chatbot = create_chatbot()
     print(f"Continuando chat {id_chat} con mensaje: {mensaje}")
-    query_result:str = chatbot.chat(text=mensaje, conversation=id_chat)  # Asegúrate de usar await aquí
+    query_result:str = chatbot.chat(text=mensaje, conversation=id_chat)
     print(f"Respuesta: {query_result}")
     return query_result  # Accede a la propiedad .text del objeto Message
 
